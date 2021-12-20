b0559945b4fbffde343300ca86887c968539b3c0
Tai Zhang <tai.zhang@outlook.com>
2021-12-20 12:42:10 +0800

    Add memory for jenkins container

--- terraform/jenkins.tf
+++ terraform/jenkins.tf
@@ -12,6 +12,8 @@ resource "proxmox_lxc" "jenkins" {
     ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDXiMs/CDEjPX7djACcECWL9zTbNTGyhto5nHMW15kTMzyjTgPETRdSD1Xh+Y1lj4IDsvUZK3Wr6gBjA5kFIldcqB7WtyESWm2hOk1yOhmTJUqeRjdIZB4yWd8YJxLqQxwD0Rglay+fq0LFGm/TaXvgAHl0HctkRmd+yXV6nru1vO0q6k2ASkAn18KC8SPXgZrDzx9jjA9sfyuYb7B+OHCL/o0WJ/7i+pIVYOBoE3ucjReRznY0E/P22IB+owtwq1E0XENjmzgSKNfmZC38n5uFmOGJDfJFTJhHMzEi/15ym+rWSy4BjjOxV4WIE1b9u/OPdgf8HvfpOcaI1X5KK7t7l4aSYFj2pInAu0gfwC51ImaEflc2GuWWrLGUrwp9eYHEazv++SedP5aCR9qm65fcxsbCyACCYWbAOpxcmUb9D5wMR9A17aDJFO6Yk6/5LLGlm/s1J2b96X8m4y0vNpi8vI6W+KrnUaDazj2UMbtathQsciNIMM9udrUZnE5XLm9JI+rW+8qkc+aXwmSQY0xSREtRgDDfF8828eeeibX+PlA1lbZixbPKp6NyPY7Rw3YszSwr259fB1Ib9aCEqNN+bJlhu/fJm71VHFaMCa6gUCqpu9c3+vcI1qkFkCw3W0IifMVNlI50DDtXN8abwRGziENx913bzZPjhjmN3GIkYQ== taizhang@Tais-MBP.lan
   EOT
 
+  memory       = 2048
+
   rootfs {
     storage = "local-lvm"
     size    = "30G"
